9eacf6931b61d308e297a717c437d32074c4fb20
SANJUANADH <97081147+SANJUANADH@users.noreply.github.com>
2022-10-15 20:48:11 +0530

    Add files via upload

---
+++ ipl.py
@@ -0,0 +1,35 @@
+import altair as alt
+import pandas as pd
+import numpy as np
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(
+    page_title="IPL Match Data", page_icon="⬇", layout="centered"
+)
+
+
+
+df = pd.read_csv('matches.csv')
+df.head(10)
+
+
+alt.Chart(df).mark_bar().encode(
+    x='season',
+    y='win_by_runs',
+    color = 'toss_decision'
+)
+
+
+# plotting the line chart
+fig = px.pie(df, values="win_by_wickets", names="season")
+ 
+# showing the plot
+fig.show()
+
+
+# plotting the line chart
+fig = px.pie(df, values="win_by_runs", names="season")
+ 
+# showing the plot
+fig.show()
