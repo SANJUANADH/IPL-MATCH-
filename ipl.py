f43b00d88480d3d8f1e1d60e8452aecdfefec62c
SANJUANADH <97081147+SANJUANADH@users.noreply.github.com>
2022-10-15 22:09:55 +0530

    Update ipl.py

--- ipl.py
+++ ipl.py
@@ -37,3 +37,22 @@ fig = px.pie(df, values="win_by_runs", names="season")
 # showing the plot
 #fig.show()
 st.plotly_chart(fig, use_container_width=True)
+
+st.subheader('Histogram - Winning Wickets')
+san = px.histogram(df, x="winner", y="win_by_wickets")
+st.plotly_chart(san, use_container_width=True)
+
+
+st.subheader('Histogram - Winning Runs')
+san = px.histogram(df, x="winner", y="win_by_runs")
+st.plotly_chart(san, use_container_width=True)
+
+
+
+
+st.subheader('3D scatter')
+sun = px.scatter_3d(df, x = 'toss_winner',
+                    y = 'winner',
+                    z = 'season',
+                    color = 'toss_decision')
+st.plotly_chart(sun, use_container_width=True)
